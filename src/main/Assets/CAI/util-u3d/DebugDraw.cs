2a0cf5a408cdfb8718c869f2aa17f7ea23ecd968
dantreble <dan@defiantdev.com>
2015-06-11 14:33:00 +1000

    Fix line material for 5.1

--- src/main/Assets/CAI/util-u3d/DebugDraw.cs
+++ src/main/Assets/CAI/util-u3d/DebugDraw.cs
@@ -92,13 +92,7 @@ namespace org.critterai.u3d
             {
                 if (!mSimpleMaterial)
                 {
-                    mSimpleMaterial = new Material(
-                        "Shader \"Lines/Colored Blended\" {"
-                        + "SubShader { Pass { "
-                        + "	BindChannels { Bind \"Color\",color } "
-                        + "	Blend SrcAlpha OneMinusSrcAlpha "
-                        + "	ZWrite Off Cull Off Fog { Mode Off } "
-                        + "} } }");
+                    mSimpleMaterial = new Material(Shader.Find("Hidden/Internal-Colored"));
                     mSimpleMaterial.hideFlags = HideFlags.HideAndDontSave;
                     mSimpleMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
                 }
